b465f5bbd35ddb8534390b643c5d9a156965ea6c
Nhat <Nhatnguyen915@gmail.com>
2016-06-06 12:09:57 +0700

    dsdsd
    dsdsdsdsd

--- Cdimex/index.php
+++ Cdimex/index.php
@@ -61,143 +61,124 @@
 		<div class="row">
 			<!-- start header -->
 <<<<<<< HEAD
-			<div class="container">	
-				<div class="row">
-					<nav class="navbar navbar-default navbar-fixed-top">
-					  <div class="container">
-
-					    <!-- Collect the nav links, forms, and other content for toggling -->
-					    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					      <ul class="nav navbar-nav">
-					        <li><a href="show_cart.php"><img src="image/icon/images.png" height="20"><b> Giỏ Hàng <span class="badge">0
-						    </span></b></a></li>
-					      </ul>
-					      <form class="navbar-form navbar-left" role="search">
-					        <div class="input-group">
-					          <tr>
-					          	<input type="text" class="form-control" placeholder="Search" size="50%">
-					          	<span class="input-group-btn"><button type="submit" class="btn btn-primary">Tìm</button></span>
-					          </tr>
-					        </div>
-					      </form>
-					      <ul class="nav navbar-nav navbar-right">
-					        <li class="dropdown">
-					          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><font color="#0000FF"><b>Đăng Nhập<b><span class="caret"></span></font></span></a>
-					          <ul class="dropdown-menu">
-					            <li><a class="btn btn-link btn-sl" data-toggle="modal" data-target="#myModal"><image src="image/icon/dn.jpg" width="20" height="20">Đăng Nhập</a></li>
-					             <li><a class="btn btn-link btn-sl" data-toggle="modal" data-target="#myModal"><image src="image/icon/dk.png" width="20" height="20">   Đăng Ký</a></li>
-					          </ul>
-					        </li>
-					      </ul>
-					    </div><!-- /.navbar-collapse -->
-					  </div><!-- /.container-fluid -->
-					</nav>
-				</div> 
-				<div class="row container-fuild">
-					<div class="navbar-right col-xs-10">
-						<div id="myCarousel" class="carousel slide" data-ride="carousel">
-						  <!-- Indicators -->
-						  <ol class="carousel-indicators">
-						    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-						    <li data-target="#myCarousel" data-slide-to="1"></li>
-						    <li data-target="#myCarousel" data-slide-to="2"></li>
-						    <li data-target="#myCarousel" data-slide-to="3"></li>
-						  </ol>
-
-						  <!-- Wrapper for slides -->
-						  <div class="carousel-inner" role="listbox">
-						    <div class="item active">
-						      <img src="image/slideshow/hinh1.jpg" alt="Chania">
-						    </div>
-
-						    <div class="item">
-						      <img src="image/slideshow/hinh2.jpg" alt="Chania">
-						    </div>
-
-						    <div class="item">
-						      <img src="image/slideshow/hinh3.jpg" alt="Flower">
-						    </div>
-						  </div>
-
-						  <!-- Left and right controls -->
-						  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-						    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-						    <span class="sr-only">Previous</span>
-						  </a>
-						  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-						    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-						    <span class="sr-only">Next</span>
-						  </a>
-						</div>
-					</div>
-					<div id="cssmenu" class="navbar-left col-xs-2">
-								<p class="danhmuc">Danh Mục</p>
-								<ul>
-								   <li class="active"><a href='index.html'><span>Home</span></a></li>
-								   <li class="has-sub"><a href='#'><span>Products</span></a>
-								      <ul>
-								         <li class="has-sub"><a href='#'><span>Product 1</span></a>
-								            <ul>
-								               <li><a href='#'><span>Sub Item</span></a></li>
-								               <li><a href='#'><span>Sub Item</span></a></li>
-								            </ul>
-								         </li>
-								         <li class="has-sub"><a href='#'><span>Product 2</span></a>
-								            <ul>
-								               <li><a href='#'><span>Sub Item</span></a></li>
-								               <li><a href='#'><span>Sub Item</span></a></li>
-								            </ul>
-								         </li>
-								      </ul>
-								   </li>
-								   <li><a href='#'><span>About</span></a></li>
-								   <li><a href='#'><span>Contact</span></a></li>
-								   <li><a href='#'><span>About</span></a></li>
-								   <li class="has-sub"><a href='#'><span>Products</span></a>
-								      <ul>
-								         <li class="has-sub"><a href='#'><span>Product 1</span></a>
-								            <ul>
-								               <li><a href='#'><span>Sub Item</span></a></li>
-								               <li><a href='#'><span>Sub Item</span></a></li>
-								            </ul>
-								         </li>
-								         <li class="has-sub"><a href='#'><span>Product 2</span></a>
-								            <ul>
-								               <li><a href='#'><span>Sub Item</span></a></li>
-								               <li><a href='#'><span>Sub Item</span></a></li>
-								            </ul>
-								         </li>
-								      </ul>
-								   </li>
-								   <li><a href='#'><span>About</span></a></li>
-								</ul>
-					</div>
-				</div>
-			</div>
 			
 =======
+>>>>>>> origin/master
 			<?php
 				include("Views/Header.php"); 
 			?>
->>>>>>> origin/master
 			<!-- end header -->
-			<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<!-- modal -->
+			<!-- modal Login -->
+			<div class="modal" id="modallogin" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 				  <div class="modal-dialog">
 				    <div class="modal-content">
 				      <div class="modal-header">
 				        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-				        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+				        <h3  align="center" class="modal-title" id="myModalLabel"><image src="image/icon/login.png" width="30" height="30">  Đăng Nhập</h3>
+				       <p align="center">Nếu bạn chưa có tài khoản, <a href="#" align="left" data-toggle="modal" data-target="#modalpugin">Đăng Ký</a><p>
 				      </div>
 				      <div class="modal-body">
-				        ...
+							<form class="form-horizontal" role="form">
+								  <div class="form-group">
+								    <label for="inputEmail3" class="col-sm-4 control-label"> Nhập Email</label>
+								    <div class="col-sm-6">
+								      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+								    </div>
+								  </div>
+								  <div class="form-group">
+								    <label for="inputPassword3" class="col-sm-4 control-label"> Nhập Password</label>
+								    <div class="col-sm-6">
+								      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+								    </div>
+								  </div>
+								  <div class="form-group">
+								    <div class="col-sm-offset-2 col-sm-10">
+								      <div class="checkbox">
+								        <label>
+								          <input type="checkbox"> Lưu tài khoản
+								        </label>
+								      </div>
+								    </div>
+								  </div>
+								  <div class="form-group">
+								    <div class="col-sm-offset-2 col-sm-10">
+								      <button type="button" class="btn btn-link" data-dismiss="modal">Quên mật khẩu</button>
+				       				 <button type="button" class="btn btn-primary">Đăng Nhập</button>
+								    </div>
+								  </div>
+							</form>
 				      </div>
 				      <div class="modal-footer">
-				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				        <button type="button" class="btn btn-primary">Save changes</button>
+				       
 				      </div>
 				    </div>
 				  </div>
 				</div>
+				<!-- modal dang ky-->
+				<div class="modal " id="modalpugin" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				  <div class="modal-dialog">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				        <h3  align="center" class="modal-title" id="myModalLabel"><image src="image/icon/login.png" width="30" height="30">  Đăng ký tài khoản</h3>
+				        <p align="center">Nếu bạn đã có tài khoản, <a href="#"data-toggle="modal" data-target="#modallogin" >Đăng Nhập </a><p>
+				      </div>
+				      <div class="modal-body">
+							<form class="form-horizontal" role="form">
+								  <div class="form-group">
+								    <label for="inputEmail3" class="col-sm-4 control-label">Email</label>
+								    <div class="col-sm-6">
+								      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+								    </div>
+								  </div>
+								  <div class="form-group">
+								    <label for="inputPassword3" class="col-sm-4 control-label">Mật Khẩu</label>
+								    <div class="col-sm-6">
+								      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+								    </div>
+								  </div>
+								  <div class="form-group">
+								    <label for="inputPassword3" class="col-sm-4 control-label">Họ và tên</label>
+								    <div class="col-sm-6">
+								      <input type="password" class="form-control" id="inputname" placeholder="name">
+								    </div>
+								  </div>
+								  <div class="form-group">
+								    <label for="inputPassword3" class="col-sm-4 control-label">Giới tính</label>
+								    <div class="col-sm-6">
+								      <input type="password" class="form-control" id="inputsex" placeholder="Sex">
+								    </div>
+								  </div>
+								  <div class="form-group">
+								    <label for="inputPassword3" class="col-sm-4 control-label">Ngày sinh</label>
+								    <div class="col-sm-6">
+								      <input type="password" class="form-control" id="inputNS" placeholder="NS">
+								    </div>
+								  </div>
+								   <div class="col-sm-offset-2 col-sm-10">
+								      <div class="checkbox">
+								        <label>
+								          <input type="checkbox">
+								          <h4> <font size="2">Nhan Cac Thong Tin Va Chuong Trinh Khuyen Mai Qua Email</font></h4>
+								          <h4><font size="2">Khi bạn nhấn Đăng ký, bạn đã đồng ý thực hiện mọi giao dịch mua bán theo điều kiện sử dụng và chính sách của Tiki.</font></h4>
+								        </label>
+								      </div>
+								    </div>
+								  <div class="form-group">
+								    <div class="col-sm-offset-4 col-sm-10">
+				       				 <button type="button" class="btn btn-primary">Đăng Ký</button>
+								    </div>
+								  </div>
+							</form>
+				      </div>
+				      <div class="modal-footer">
+				       
+				      </div>
+				    </div>
+				  </div>
+				</div>
+			</div>
 			<!-- start content -->
 			<?php
 				include("Views/Content.php");
