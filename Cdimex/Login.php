899cd63d4350b451bf8728d31247543a67614612
Nhat <Nhatnguyen915@gmail.com>
2016-06-24 11:21:43 +0700

    dang nhap
    dang nhap

--- Cdimex/Login.php
+++ Cdimex/Login.php
@@ -55,33 +55,74 @@
 				      <div class="modal-body" name="dk">
 							<form class="form-horizontal" role="form" name="dk">
 								  <div class="form-group">
-								    <label for="inputEmail3" class="col-sm-4 control-label"> Nhập Email</label>
+								    <label for="inputEmail3" class="col-sm-3 control-label"> Nhập Email</label>
 								    <div class="col-sm-6">
 								      <input type="email" class="form-control" id="email" placeholder="Email">
 								    </div>
 								  </div>
 								  <div class="form-group">
-								    <label for="inputPassword3" class="col-sm-4 control-label"> Nhập Password</label>
+								    <label for="inputPassword3" class="col-sm-3 control-label"> Nhập Password</label>
 								    <div class="col-sm-6">
 								      <input type="password" class="form-control" id="password" placeholder="Password">
 								    </div>
 								  </div>
 								  <div class="form-group">
-								    <label for="inputPassword3" class="col-sm-4 control-label">Họ và tên</label>
+								    <label for="inputPassword3" class="col-sm-3 control-label">Họ và tên</label>
 								    <div class="col-sm-6">
-								      <input type="password" class="form-control" id="inputname" placeholder="Name">
+								      <input type="text" class="form-control" id="inputname" placeholder="Name">
 								    </div>
 								  </div>
 								  <div class="form-group">
-								    <label for="inputPassword3" class="col-sm-4 control-label">Giới tính</label>
-								    <div class="col-sm-6">
-								      <input type="password" class="form-control" id="inputsex" placeholder="Sex">
+								    <label for="inputPassword3" class="col-sm-3 control-label">Giới tính</label>
+								    <div class="col-sm-3">
+								      <select class="input-large form-control">
+								      		 <option value="Nam" selected="selected">Nam</option>
+							                 <option value="Nữ">Nữ</option>
+								       </select>
 								    </div>
 								  </div>
 								  <div class="form-group">
-								    <label for="inputPassword3" class="col-sm-4 control-label">Ngày sinh</label>
-								    <div class="col-sm-6">
-								      <input type="password" class="form-control" id="inputNS" placeholder="Birthday">
+								    <label for="inputPassword3" class="col-sm-3 control-label">Ngày sinh</label>
+								    <div class="col-sm-3">
+								       <select class="input-large form-control">
+							                    <option value="0" selected="selected">Ngày</option>
+							                    <?php
+							                    	for($i=1;$i<=31;$i++)
+							                    	{
+							                    ?>
+							                    		<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+							                    <?php	
+							                    	}
+							                    ?>
+							                </select>
+								    </div>
+								     <div class="col-sm-3">
+								       <select class="input-large form-control">
+							                    <option value="0" selected="selected">Tháng</option>
+							                    <?php
+							                    	for($i=1;$i<=12;$i++)
+							                    	{
+							                    ?>
+							                    		<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+							                    <?php	
+							                    	}
+							                    ?>
+							                </select>
+								    </div>
+								     <div class="col-sm-3">
+								       <select class="input-large form-control">
+							                    <option value="0" selected="selected">Năm</option>
+							                    <?php
+							                    	for($i=2016;$i>=1900;$i--)
+							                    	{
+							                    ?>
+							                    		<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+							                    <?php	
+							                    	}
+							                    ?>
+							                    
+							                    
+							                </select>
 								    </div>
 								  </div>
 								   <div class="col-sm-offset-2 col-sm-10">
