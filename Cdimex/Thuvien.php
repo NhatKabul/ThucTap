f4a1652d64896d3b4e87cfe669ba53ac7531321a
ngochung861 <ngochung861@gmail.com>
2016-06-24 10:26:31 +0700

    continueted

---
+++ Cdimex/Thuvien.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="icon" href="image/logo.ico">
+<title>Cdimex</title>
+<link rel="stylesheet" href="css/bootstrap-theme.css" type="text/css">
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="css/css.css" type="text/css">
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="css/styles.css" type="text/css">
+<link rel="stylesheet" href="css/custom.css" type="text/css">
+<link rel="stylesheet" href="css/non-responsive.css" type="text/css">
+<link rel="stylesheet" type="text/css" href="css/slick/slick.css">
+<link rel="stylesheet" type="text/css" href="css/slick/slick-theme.css">
+<script src="js/batloi-dk.js" type="text/javascript"></script>
+<script src="js/jquery-2.2.2.min.js" type="text/javascript"></script>
+<script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script src="css/slick/slick.js" type="text/javascript" charset="utf-8"></script>
+
+  <style type="text/css">
+    html, body {
+      margin: 0;
+      padding: 30px 0px;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+
+    .slider {
+        width: 100%;
+        margin: 0px;
+        top: 10px;
+        left: 0px;
+    }
+
+    .slick-slide {
+      margin: 0px 20px;
+    }
+
+    .slick-slide img {
+      width: 100%;
+    }
+
+    .slick-prev:before,
+    .slick-next:before {
+        color: #0380ff;
+    }
+  </style>
+</head>
+<body>
+    <div class="container">
+    	<div class="nen row navbar navbar-default"><!-- banner -->
+		  <div class="col-xs-2 row"><a href="index.php"><img src="image/logo/logo.png" width="100%" height="100%"></a></div>
+		  <div class="col-xs-10">
+		  </div>
+		</div><!-- banner -->
+		
