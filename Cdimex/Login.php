3c8a5d56a7a93bd3bd3aad42205628da7698f821
Nhat <Nhatnguyen915@gmail.com>
2016-06-27 11:24:03 +0700

    gg

--- Cdimex/Login.php
+++ Cdimex/Login.php
@@ -1,3 +1,4 @@
+
 			<div class="modal" id="modallogin" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 				  <div class="modal-dialog">
 				    <div class="modal-content">
@@ -11,13 +12,13 @@
 								  <div class="form-group">
 								    <label for="inputEmail3" class="col-sm-4 control-label"> Nhập Email</label>
 								    <div class="col-sm-6">
-								      <input type="email" class="form-control" id="email" placeholder="Email">
+								      <input type="email" class="form-control" id="txtID" placeholder="Email" >
 								    </div>
 								  </div>
 								  <div class="form-group">
 								    <label for="inputPassword3" class="col-sm-4 control-label"> Nhập Password</label>
 								    <div class="col-sm-6">
-								      <input type="password" class="form-control" id="password" placeholder="Password">
+								      <input type="password" class="form-control" id="txtPW" placeholder="Password" >
 								    </div>
 								  </div>
 								  <div class="form-group">
@@ -32,7 +33,7 @@
 								  <div class="form-group">
 								    <div class="col-sm-offset-2 col-sm-10">
 								      <button type="button" class="btn btn-link">Quên mật khẩu</button>
-				       				 <button type="submit" class="btn btn-primary">Đăng Nhập</button>
+				       				 <button type="submit" class="btn btn-primary" name ="submit">Đăng Nhập</button>
 								    </div>
 								  </div>
 							</form>
@@ -137,7 +138,7 @@
 								  <div class="form-group">
 								    <div class="col-sm-offset-2 col-sm-10">
 								      <button type="button" class="btn btn-link">Quên mật khẩu</button>
-				       				 <button type="submit" id="submit" class="btn btn-primary">Đăng Ký</button>
+				       				 <button type="submit" id="submit" class="btn btn-primary" name="submit">Đăng Ký</button>
 								    </div>
 								  </div>
 							</form>
